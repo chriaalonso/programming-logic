e9f766f9be9f5fce2c03a843f40eccd02609896d
chriaalonso <chriaalonso@gmail.com>
2023-06-22 08:30:58 -0300

    Divisão com TS

---
+++ TS/divisao/divisao.ts
@@ -0,0 +1,11 @@
+let divisor: number = 7;
+let dividendo: number = 3 
+let resultado: number;
+let quociente: number;
+
+resultado = divisor / dividendo;
+quociente = divisor % dividendo;
+
+console.log(resultado);
+console.log(quociente);
+
